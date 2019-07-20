5d120ac4ed70399ff53c3958f34413ceb9ae75df
nakuYK <395126376@qq.com>
2019-07-20 11:14:45 +0800

    wx.MiniGame commit

---
+++ js/player/StartButton.js
@@ -0,0 +1,18 @@
+//开始按钮类
+import {Sprite} from "../base/Sprite.js";
+import {DataStore} from "../base/DataStore.js";
+
+export class StartButton extends Sprite {
+    constructor() {
+        const image = Sprite.getImage('startButton');
+        super(
+            image,
+            0, 0,
+            image.width, image.height,
+            (DataStore.getInstance().canvas.width - image.width) / 2,
+            (DataStore.getInstance().canvas.height) / 3,
+            image.width, image.height
+        );
+
+    }
+}
